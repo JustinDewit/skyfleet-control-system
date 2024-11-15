f81429c20fb513f5d5accb28702f7f2770a0de5d
JustinDewit <137382666+JustinDewit@users.noreply.github.com>
2024-11-15 09:46:59 +0100

    chore: deps

--- src/app/api/map-style/route.ts
+++ src/app/api/map-style/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const apiKey = process.env.MAPTILER_API_KEY;
-  const response = await fetch(`https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`);
+  const response = await fetch(`https://api.maptiler.com/maps//style.json?key=${apiKey}`);
   const data = await response.json();
   return NextResponse.json(data);
 }
